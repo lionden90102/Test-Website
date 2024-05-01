db25b37d9b58a54616fc4edd1c4b36bc70477470
lionden90102 <141382750+lionden90102@users.noreply.github.com>
2024-05-01 03:22:02 -0700

    Update and rename Login.html to Login.js

---
+++ Login.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyC_L2wj-kkkBhwbxihWZkTAki7EJbgcWNk",
+    authDomain: "login-d7c37.firebaseapp.com",
+    projectId: "login-d7c37",
+    storageBucket: "login-d7c37.appspot.com",
+    messagingSenderId: "461503111575",
+    appId: "1:461503111575:web:9df56ac00cd6be9b7fe4d2",
+    measurementId: "G-NMEEMSPD9P"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
